c19f2b99bbe59abc82bc2f0b7ff130e39bb9eeb5
ImBroke40 <107781604+ImBroke40@users.noreply.github.com>
2022-06-22 10:32:54 -0400

    Update first.py
    So the issue is whenever you answer No to the question - print("Uhm- Uh, hello? Is this the uh- Suicide Prevention Hotline or-?")
        
        Q1A = input("Type Yes or No to continue ") - it skips all the code where it says - if Q1A == "No" or Q1A == "no":
            print("Oh... Sorry I'll try a different num- you know what, nevermind, it's easier to finish it this way. *Hangs up*")
            time.sleep(2)
            print("The next day you sit down on your leather couch, turn on the tv, and are greeted with the famaliar sound of the news coverage, it goes on for what seems like forever, but then, the screen turns pure black, \n not the shitty dark blue it turns to when it's turned off, but pure black, you get up to check on the router to see if the wifi is down, but as soon as you do, the tv switches to a breaking news channel.") 
            time.sleep(4)
            print(" TV REPORTER: [Late this evening in North Jester, 34 year old Jessica McCaine has had an unholy ending to her 17 year careeer as an actress.]")
            time.sleep(2)
            print("{SUBTITLES: THESE NEXT FEW PICTURES CONTAIN GRAPHIC AND MATURE CONTENT, VIEWER DISCRETION IS ADVISED.} \n The first picture on the screen, showing her hanging from a rope that is tied to the bars on her balcony. \n Her head and eyes rolled back, staring right at the sky. Her mouth, hastily sewn shut, small streaks of blood leaking from the holes on her lips.") - and just exits any help is appreciated, thx!

--- first.py
+++ first.py
@@ -3,7 +3,7 @@ import sys
 import webbrowser
 a_website = "https://pastebin.com/1GJqwYM8"
 webbrowser.open_new(a_website)
-time.sleep(8)
+time.sleep(5)
 print("Hello New User")
 time.sleep(1)
 USERNAME = input("Please Create A USERNAME New User ")
@@ -16,7 +16,7 @@ print("I Am Called SAHB, Pronounced SAB, I Am A Sentient OS That Coverses With O
 time.sleep(1)
 print("" + USERNAME + " You Are Part Of The Family Called Homo Sapiens")
 time.sleep(2)
-print("Type Good, Bad, or Okay On This Next Question To Continue")
+print("Type Good, Bad, or Okay/Ok On This Next Question To Continue")
 print("How Is Your Day So Far " + USERNAME + "? ")
 print()
 
@@ -26,7 +26,7 @@ if GBOO == "Good" or GBOO == "good":
     print("ANS 1 COMFIRMED")
 elif GBOO == "Bad" or GBOO == "bad":
     print("ANS 2 CONFIRMED")
-elif GBOO == "Okay" or GBOO == "okay":
+elif GBOO == "Okay" or GBOO == "okay" or GBOO == "Ok" or GBOO == "ok":
     print("ANS 3 CONFIRMED")
 else:
     print("SAHB OS DOES NOT RECOGNIZE ANS")
@@ -57,16 +57,52 @@ REDIAL = input(" {Type Start, Exit 2 Desktop, or I Don't Want To Play To Continu
 if REDIAL == "Start" or REDIAL == "start":
     print("ANS 1 COMFIRMED")
     time.sleep(1)
-elif REDIAL == "Exit 2 Desktop" or REDIAL == "exit 2 desktop":
-    print("ANS 2 CONFIRMED")
+    print("STARTING IN 3...")
+    time.sleep(1)
+    print("2...")
+    time.sleep(1)
+    print("1...")
+    time.sleep(1)
+    print("Uhm- Uh, hello? Is this the uh- Suicide Prevention Hotline or-?")
+    
+    Q1A = input("Type Yes or No to continue ")
+
+if Q1A == "Yes" or Q1A == "yes":
+        print("Well then I'm pretty sure you can guess why I'm calling...")
+        time.sleep(2)
+        print("I uh- I don't think I'm going to- to uh, make it? I guess I could say?")
+        time.sleep(5)
+        print("This is uh- the usual for you then huh? Odd, How do you feel when you- talk to someone like me?")
+                    
+        Q2A = input("Type Sad, Content, or Unfazed to continue. YOUR ANS: ")
+
+if Q2A == "Sad" or Q2A == "sad":
+    print( + USERNAME + ": It's very sad to see others attempting to take their own lives when adults have to call 911 because someone jumped off their balcony, when children have to see someone hang themself, it's just sad, really.")
+elif Q2A == "Content" or Q2A == "content":
+    print( + USERNAME + ": I'm happy that people like you are trying to pick themselves back up and put their life back together, it's... enlightening.")
+elif Q2A == "Unfazed" or Q2A == "unfazed":
+    print("" + USERNAME + ": I feel fine, however, people like you, who have had many hard times in their lives, are making this job be able to support families of the people who answer the calls. \n YOU, are what powers these families, YOU, are what makes this job exist. So do what you will, but take a chance and think.")
+else:
+    print("SAHB OS DOES NOT RECOGNIZE ANS")
+    time.sleep(2.5)
     sys.exit ()
+    
+if Q1A == "No" or Q1A == "no":
+        print("Oh... Sorry I'll try a different num- you know what, nevermind, it's easier to finish it this way. *Hangs up*")
+        time.sleep(2)
+        print("The next day you sit down on your leather couch, turn on the tv, and are greeted with the famaliar sound of the news coverage, it goes on for what seems like forever, but then, the screen turns pure black, \n not the shitty dark blue it turns to when it's turned off, but pure black, you get up to check on the router to see if the wifi is down, but as soon as you do, the tv switches to a breaking news channel.") 
+        time.sleep(4)
+        print(" TV REPORTER: [Late this evening in North Jester, 34 year old Jessica McCaine has had an unholy ending to her 17 year careeer as an actress.]")
+        time.sleep(2)
+        print("{SUBTITLES: THESE NEXT FEW PICTURES CONTAIN GRAPHIC AND MATURE CONTENT, VIEWER DISCRETION IS ADVISED.} \n The first picture on the screen, showing her hanging from a rope that is tied to the bars on her balcony. \n Her head and eyes rolled back, staring right at the sky. Her mouth, hastily sewn shut, small streaks of blood leaking from the holes on her lips.")
+        
+        if REDIAL == "Exit 2 Desktop" or REDIAL == "exit 2 desktop":
+            print("ANS 2 CONFIRMED")
+            sys.exit ()
 elif REDIAL == "I Don't Want To Play" or REDIAL == "i don't want to play" or REDIAL == "i dont want to play" or REDIAL == "I Dont Want To Play":
     print("ANS 3 CONFIRMED")
     time.sleep(1)
-    print("EXITING REDIAL SERVICES")
-    time.sleep(0.5)
-    print("RESTARTING SAHB OS SYSTEM")
-    sys.exit ()
+    print("We Can Talk About Something Else Then " + USERNAME + ".")
 else:
     print("SAHB OS DOES NOT RECOGNIZE ANS")
     time.sleep(2.5)
